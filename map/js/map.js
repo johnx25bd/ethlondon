4defd361c0bf0700ecc7ba8f53fb70ab812533c6
John IV <john.r.hoopes@gmail.com>
2020-03-01 01:04:42 +0000

    some data

--- map/js/map.js
+++ map/js/map.js
@@ -3,10 +3,68 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iaXNvbml2IiwiYSI6ImNqbjM5eXEwdjAyMnozcW9jM
 var color = d3.schemeSet2;
 var map = new mapboxgl.Map({
   container: 'zone-map',
-  style: 'mapbox://styles/mapbox/light-v10',
+  style: 'mapbox://styles/mapbox/dark-v9',
   center: [
     14.038784600499525,
     49.29969274777156
   ],
-  zoom: 2.7,
+  zoom: 5.7,
+  pitch: 70, // pitch in degrees
 });
+
+
+map.on('style.load', function () {
+  map.style.stylesheet.layers.forEach(function(layer) {
+    if (layer.type === 'symbol') {
+      map.removeLayer(layer.id);
+    }
+  });
+
+  // map.addSource('london-source', {
+  //   "type": 'geojson',
+  //   'data': geojson[0]
+  // });
+  //
+  // map.addLayer({
+  //   'id': 'london',
+  //   'type': 'fill',
+  //   'source': 'london-source',
+  //   'layout': {},
+  //   'paint': {
+  //     'fill-color': 'lightblue',
+  //     'fill-opacity': 0.2
+  //   }
+  // });
+  //
+  // map.addSource('dover-source', {
+  //   "type": 'geojson',
+  //   'data': geojson[1]
+  // });
+  //
+  // map.addLayer({
+  //   'id': 'dover',
+  //   'type': 'fill',
+  //   'source': 'dover-source',
+  //   'layout': {},
+  //   'paint': {
+  //     'fill-color': 'red',
+  //     'fill-opacity': 0.4
+  //   }
+  // });
+  //
+  // map.addSource('rotterdam-source', {
+  //   "type": 'geojson',
+  //   'data': geojson[2]
+  // });
+  //
+  // map.addLayer({
+  //   'id': 'rotterdam',
+  //   'type': 'fill',
+  //   'source': 'rotterdam-source',
+  //   'layout': {},
+  //   'paint': {
+  //     'fill-color': 'green',
+  //     'fill-opacity': 0.4
+  //   }
+  // });
+})
